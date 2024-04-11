aadcf83956f4bf05bd7845f69106243a2d4ca8ae
stark <serj.elessar@gmail.com>
2024-04-11 19:11:19 +0300

    +

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,31 +1,49 @@
 import { Fragment, useEffect, useRef, useState } from 'react';
 import './App.scss';
-// import { Message } from './components/message';
+import { Message } from './components/message';
+
+// type DATA = {
+// 	message: string;
+// 	id: string;
+// 	isMe: boolean;
+// }
 
 function App() {
+	const [messages, setMessages] = useState<any[]>([]);
     const [message, setMessage] = useState('');
     const ES = useRef<EventSource | null>(null);
 
 	const sendMessage = async () => {
-		const response = await fetch('http://localhost:5238/send-text', {
-			method: 'POST',
-			headers: {'content-type': 'application/json'},
-			body: JSON.stringify({message})
-		});
-		const json = await response.json();
-		setMessage(json);
+		try {
+			const response = await fetch('http://localhost:5238/send-text', {
+				method: 'POST',
+				headers: {'content-type': 'application/json'},
+				body: JSON.stringify({message})
+			});
+			const json = await response.json();
+			setMessages(prevState => prevState.concat(json));
+		}
+		catch (error) {
+			console.log('fetch Error', error);
+		}
+		finally {
+			setMessage('');
+		}
 	}
 
     useEffect(() => {
 	    ES.current = new EventSource('http://localhost:5238/');
 
-		ES.current?.addEventListener('error', (event) => {
+		ES.current.onopen = (event) => {
+			console.log('Open source', event);
+		};
+
+		ES.current.onerror = (event) => {
 			console.log("Error:", event);
-		});
+		};
 
 	    ES.current.onmessage = (event) => {
-		    const item = JSON.parse(event.data);
-		    console.log("New item received:", item);
+		    console.log("New item received:", event);
 	    };
 
         return () => {
@@ -38,6 +56,9 @@ function App() {
 			<div className="chat">
 				<header className="chat-header">Chat</header>
 				<main className="chat-body">
+					{messages?.map((item, index) => (
+						<Message key={index} message={item} isMe={false} />
+					))}
 				</main>
 				<footer className="chat-footer">
 					<textarea className="textarea" value={message} onChange={({ target }) => setMessage(target.value)}/>

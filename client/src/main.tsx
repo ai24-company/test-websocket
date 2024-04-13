19f6f1d5dc898da34c51b7888d62fd7ed51c5fb8
stark <serj.elessar@gmail.com>
2024-04-13 13:53:09 +0300

    +

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,7 +1,12 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import './index.css';
+import ReactDOM from 'react-dom/client';
+import { Provider } from 'react-redux';
+
+import App from './App.tsx';
+import { store } from './store';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-    <App />,
-)
+	<Provider store={store}>
+		<App/>,
+	</Provider>
+);
